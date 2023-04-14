103f333e5e3191bf78ef7579e95299fc5761f80b
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-04-14 15:52:17 +0530

    commit

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Animal {}
+class Animal {
+    constructor(species){
+        // super(species)
+            this.species="";
+    }
+    get getmakeSound(){
+       return  this.species;
+    }
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+    constructor(species){
+        this.species="";
+    }
+    bark(){
+        return "woof";
+    }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+    constructor(species){
+        this.species="";
+    }
+    purr(){
+        return "purr";
+    }
+}
+ let animal=new Animal("Human");
+ console.log(animal.bark())
+ console.log(animal.purr())
 
 // Do not change the code below this line
 window.Animal = Animal;
